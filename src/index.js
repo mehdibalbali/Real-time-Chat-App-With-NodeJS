5c60e84df6510ba1868cf590dd29fe92c61e0d0d
mehdibalbali <mehdibalbali@yahoo.fr>
2020-01-03 23:09:13 +0100

    some changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const path = require('path')
-const app = express()
+const http = require('http')
 
-const port = process.env.port || 3000
-const publicDirectoryPath = path.join(__dirname,'../')
+const app = express()
+const server = http.createServer(app)
+
+const port = process.env.PORT || 3100
+const publicDirectoryPath = path.join(__dirname,'../public')
+
+app.use(express.static(publicDirectoryPath))
+
+server.listen(port, () =>{
+    console.log(`Server is running on port ${port} !`)
+})
